d7adc5eaf01fb8c63aaa99e2ea39a3c53037ad29
Beconfident01 <hom0726@naver.com>
2023-03-23 10:31:35 +0900

    세팅

---
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -0,0 +1,15 @@
+package com.mysite.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+	void testJpa() {
+		Question q1 = new Question();
+		q1.setSubject("sbb2가 무엇인가요?");
+		q1.
+	}
+
+}
